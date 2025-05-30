c37d87d8df0f8b15f64b01febb06850d4dc32eb5
unknown <mattamharikacse@gmail.com>
2025-05-30 22:44:29 +0530

    Initial commit

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home'
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import { Navigate } from "react-router-dom";
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/SignUp" />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/Login" exact element={<Login />} />
       <Route path="/SignUp" exact element={<SignUp />} />
